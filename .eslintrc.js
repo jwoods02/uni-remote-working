90a193b5e94cbfa18eba934446d17d0dd657b9f9
James Woods <woodsj13@cardiff.ac.uk>
2019-02-19 14:01:59 +0000

    Removed prettier from eslint, run seperately

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es6: true
   },
-  extends: ["react-app", "plugin:prettier/recommended"],
+  extends: ["react-app", "prettier"],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly"
