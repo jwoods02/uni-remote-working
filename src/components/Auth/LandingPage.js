32425d4c99e7fbc018e1a67f4c20e88c78d9d30b
danjones457 <danjonesy@ymail.com>
2019-03-04 09:35:26 +0000

    Testing Github desktop

--- src/components/Auth/LandingPage.js
+++ src/components/Auth/LandingPage.js
@@ -9,6 +9,7 @@ const { height } = Dimensions.get("window");
 
 export default class LandingPage extends React.Component {
   static navigationOptions = { header: null };
+  test
 
   async componentWillMount() {
     await Font.loadAsync({
