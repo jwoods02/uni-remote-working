feb1ef3afc2b8ead5492b5befea7d9ed713185a1
danjones457 <danjonesy@ymail.com>
2019-03-10 16:47:37 +0000

    Home page header and map
    - Map currently shows all of the possible locations on the map
    - Styling of heading needs altering

--- src/components/Home/ActiveCodeHome.js
+++ src/components/Home/ActiveCodeHome.js
@@ -1,50 +1,81 @@
+// https://codedaily.io/tutorials/9/Build-a-Map-with-Custom-Animated-markers-and-Region-Focus-when-Content-is-Scrolled-in-React-Native
+// https://github.com/browniefed/map_animated_scrollview/blob/master/index.ios.js
+
 import React, { Component } from "react";
 import {
-  View,
-  Text,
+  AppRegistry,
   StyleSheet,
-  SafeAreaView,
-  TextInput,
-  Platform,
-  StatusBar,
-  ScrollView,
-  Image,
+  Text,
+  View,
+  Animated,
   Dimensions,
+  ActivityIndicator,
   Button,
-  TouchableOpacity
+  Alert
 } from "react-native";
-import Icon from "@expo/vector-icons/Ionicons";
-import FavouritesCarousel from "./FavouritesCarousel";
+
 import firebase from "firebase";
+import MapViewItems from "../Maps/MapComponents/MapViewItems";
 
-const { height, width } = Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
+const CARD_HEIGHT = height / 4;
+const CARD_WIDTH = CARD_HEIGHT - 50;
 
-class ActiveCodeHome extends Component {
+export const getCurrentLocation = () => {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      position => resolve(position),
+      e => reject(e)
+    );
+  });
+};
+
+export default class ActiveCodeHome extends Component {
   constructor() {
     super();
     this.ref = firebase.firestore().collection("locations");
     this.unsubscribe = null;
     this.state = {
       isLoading: true,
-      locations: []
+      markers: [],
+      region: {
+        //just a default incase snapshot fails - Cardiff
+        latitude: 51.481583,
+        longitude: -3.17909,
+        latitudeDelta: 0.04864195044303443,
+        longitudeDelta: 0.040142817690068
+      }
     };
   }
-  componentWillMount() {
-    this.startHeaderHeight = 80;
-    if (Platform.OS == "android") {
-      this.startHeaderHeight = 100 + StatusBar.currentHeight;
-    }
-  }
 
+  componentWillMount() {
+    this.index = 0;
+    this.animation = new Animated.Value(0);
+  }
   componentDidMount() {
-    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
+    this.state = this.unsubscribe = this.ref.onSnapshot(
+      this.onCollectionUpdate
+    );
+
+    getCurrentLocation().then(position => {
+      if (position) {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: 0.03,
+            longitudeDelta: 0.03
+          }
+        });
+      }
+    });
   }
 
   onCollectionUpdate = querySnapshot => {
-    const locations = [];
+    const markers = [];
     querySnapshot.forEach(doc => {
       const { title, description, image, coordinate } = doc.data();
-      locations.push({
+      markers.push({
         key: doc.id,
         title,
         description,
@@ -53,46 +84,98 @@ class ActiveCodeHome extends Component {
       });
     });
     this.setState({
-      locations,
+      markers,
       isLoading: false
     });
   };
 
+  _feedback() {
+    Alert.alert("This is where feedback will be handled");
+  }
+
+  _howTo() {
+    Alert.alert("This is where how to will be handled");
+  }
+
+  _removeCode() {
+    Alert.alert("This is where the code will be removed");
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.activity}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      );
+    }
+
     return (
-      <SafeAreaView style={{ flex: 1 }}>
-        <View style={{ flex: 1 }}>
-          <View>
+      <View style={styles.container}>
+        <View
+          style={{
+            maxHeight: 130,
+            backgroundColor: "white",
+            borderBottomWidth: 1,
+            borderBottomColor: "#dddddd",
+            paddingTop: 20,
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "space-between"
+          }}
+        >
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              justifyContent: "space-between"
+            }}
+          >
             <Button
               onPress={this._removeCode}
               title="Remove code"
               color="#FF0000"
             />
-            <Text>Cardiff Library</Text>
-            <Button
-              onPress={this._feedback}
-              title="Feedback"
-            />
+            <Text style={{ fontSize: 20 }}>Cardiff Library</Text>
+            <Button onPress={this._feedback} title="Feedback" />
           </View>
-          <View>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              justifyContent: "space-between"
+            }}
+          >
             <Text>Code</Text>
             <Text>Valid for: 24 hours</Text>
-            <Button
-              onPress={this._howTo}
-              title="How do I use this code?"
-            />
+            <Button onPress={this._howTo} title="How do I use this code?" />
           </View>
         </View>
-      </SafeAreaView>
+        <MapViewItems //the map
+          region={this.state.region}
+          markers={this.state.markers}
+          animation={this.animation}
+          navigation={this.props.navigation} // not sure why you have to pass navigation as prop to children components.
+        />
+      </View>
     );
   }
 }
-export default ActiveCodeHome;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
+    flex: 1
+  },
+  scrollView: {
+    position: "absolute",
+    bottom: 30,
+    left: 0,
+    right: 0,
+    paddingVertical: 10
+  },
+  endPadding: {
+    paddingRight: width - CARD_WIDTH
   }
 });
+
+AppRegistry.registerComponent("ActiveCodeHome", () => ActiveCodeHome);
