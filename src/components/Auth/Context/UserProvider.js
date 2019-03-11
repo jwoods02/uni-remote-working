219a3df450bed38fc874c0edee56a9b647a8cbca
elliottdaviess <elliott_davies@live.co.uk>
2019-03-11 11:25:22 +0000

    Removed comments

--- src/components/Auth/Context/UserProvider.js
+++ src/components/Auth/Context/UserProvider.js
@@ -35,38 +35,3 @@ export default class UserProvider extends React.Component {
     );
   }
 }
-
-// import * as React from "react";
-// import { UserContext } from "./user-context";
-// import firebase from "firebase";
-
-// export default class UserProvider extends React.Component {
-//   state = {
-//     user: null
-//   };
-//   //need the old set state in here
-//   setUser(user) {
-//     setState({ user });
-//   }
-
-//   componentDidMount() {
-//     firebase.auth().onAuthStateChanged(function(user) {
-//       if (user) {
-//         setUser(user);
-//       }
-//     });
-//   }
-//   render() {
-//     return (
-//       <UserContext.Provider
-//         value={{
-//           userContext: {
-//             ...this.state
-//           }
-//         }}
-//       >
-//         {this.props.children}
-//       </UserContext.Provider>
-//     );
-//   }
-// }
