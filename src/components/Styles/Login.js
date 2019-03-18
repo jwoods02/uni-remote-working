038d29a3dcc13dcf1e3d9ca62bdf52f322b92ae3
danjones457 <danjonesy@ymail.com>
2019-03-18 15:15:47 +0000

    Login page styling

---
+++ src/components/Styles/Login.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  textInput: {
+    height: 40,
+    width: "90%",
+    borderColor: "gray",
+    borderWidth: 1,
+    marginTop: 8,
+    backgroundColor: "#edeeef",
+    borderRadius: 5,
+    padding: 5
+  },
+  buttonContainer: {
+    margin: 10
+  },
+  signUp: {
+    position: "absolute",
+    bottom: 15
+  }
+});
+
+export { styles };
