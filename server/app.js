c941679feb4d0f9b882a9048ff0a5c207da43b62
Alexander Dilley <dilleyaj@cardiff.ac.uk>
2019-03-22 16:59:27 +0000

    Create user and grant user access to lock

--- server/app.js
+++ server/app.js
@@ -3,11 +3,23 @@ const bodyParser = require("body-parser");
 const stripe = require("stripe")("sk_test_ik5ZUTExOZD1iCpd9Iey4bXy");
 const app = express();
 const port = 4000;
-const lockAccessToken = "cc16e53b389f06cfd7d81ec814f96f472d96cf14a23510091a708fea05479c39";
-const lockRefreshToken = "60950336468bda2abf4f72c0be97e223577df9bfaa25c39e86d69769d80bc6b9";
+const schedule = require("node-schedule");
+const fetch = require("node-fetch");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const clientId =
+  "e40f1e718008dcd3035e17f78ad7357d0d68f3c7057b96f6de6e79b7e50cb34b";
+const clientSecret =
+  "d85630f1e3a7c4d80f562f056defb2105c86d91c9192af78957866eb5dc76ecc";
+
+let lockAccessToken =
+  "10e9dbf60ca73a56643c1e6c447af5b6253abfb5751073ee5d928c597f276b69";
+let lockRefreshToken =
+  "b80430d1dfe7712e7a1b753444fdb121a2fe8e59743c7cbb8c0feaaf8da2099a";
+
+/////////////////////////////////// STRIPE
 
 app.post("/api/pay/token", function(req, res) {
   console.log(req.body.token);
@@ -23,7 +35,7 @@ app.post("/api/pay/customer", async function(req, res) {
   res.send(customer);
 });
 
-app.post("/api/pay/subscription", async function (req, res) {
+app.post("/api/pay/subscription", async function(req, res) {
   console.log(req.body);
 
   const subscription = await stripe.subscriptions
@@ -42,65 +54,105 @@ app.post("/api/pay/subscription", async function (req, res) {
   res.send(subscription);
 });
 
+/////////////////////////////////// LOCK
 
+const generateToken = () => {};
 
+const refreshToken = () => {
+  fetch("https://smartconnectuk.devicewebmanager.com/oauth/token", {
+    method: "post",
+    body:
+      "grant_type=refresh_token" +
+      "&client_id=" +
+      clientId +
+      "&client_secret=" +
+      clientSecret +
+      "&refresh_token=" +
+      lockRefreshToken,
+    headers: { "Content-Type": "application/x-www-form-urlencoded" }
+  })
+    .then(res => res.json())
+    .then(json => {
+      console.log(json);
+      lockAccessToken = json.access_token;
+      lockRefreshToken = json.refresh_token;
+    })
+    .catch(err => console.log(err));
+};
 
-app.post("/api/lock/guest", async function (req, res) {
+const schRefreshToken = schedule.scheduleJob("*/119 * * * * ", () => {
+  refreshToken();
+});
 
-  console.log(req.body);
-  const date = new Date();
-
-
-  checkStatus = res => {
-    if (res.status >= 200 && res.status < 300) {
-      return res;
-    } else if (res.status === 401) {
-
-    const renewToken = await fetch('https://smartconnectuk.devicewebmanager.com/oauth/token', {
-        method: 'post',
-        body: 'grant_type=refresh_token client_id=e40f1e718008dcd3035e17f78ad7357d0d68f3c7057b96f6de6e79b7e50cb34b' + 
-        'client_secret=d85630f1e3a7c4d80f562f056defb2105c86d91c9192af78957866eb5dc76ecc refresh_token=' + lockRefreshToken ,
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      })
-        .then(res => res.json())
-        // .then(json => 
-          
-        //   // lockAccessToken = json.access_token;
-        //   // lockRefreshToken = json.refresh_token;
-          
-        //   );
-
-    } else {
-      throw MyCustomError(res.statusText);
-    }
+const checkStatus = res => {
+  if (res.status >= 200 && res.status < 300) {
+    return res;
+  } else if (res.status === 401) {
+    refreshToken();
+  } else {
+    return null;
   }
+};
+
+app.post("/api/lock/guest", async function(req, res) {
+  console.log(req.body);
+
+  const guestAccessLock = id => {
+    const body = {
+      attributes: {
+        accessible_id: "28992f53-7f92-4101-b1b5-1bf1fca693dc",
+        accessible_type: "lock"
+      }
+    };
+
+    const grantLockAccess = fetch(
+      "https://api.remotelock.com/access_persons/" + id + "/accesses",
+      {
+        method: "post",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/vnd.lockstate+json; version=1",
+          Authorization: "Bearer " + lockAccessToken
+        }
+      }
+    )
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+      })
+      .catch(err => console.error(err));
+  };
 
   const body = {
     type: "access_guest",
     attributes: {
       starts_at: new Date(),
-      ends_at: date.setDate(date.getDate() + 1),
-      name: req.user,
-      pin: req.pin
+      ends_at: new Date(new Date().getTime() + 60 * 60 * 24 * 1000),
+      name: req.body.name,
+      pin: req.body.pin
     }
   };
 
-  const createGuest = await fetch('https://api.remotelock.com/access_persons', {
-    method: 'post',
+  const createGuest = fetch("https://api.remotelock.com/access_persons", {
+    method: "post",
     body: JSON.stringify(body),
     headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/vnd.lockstate+json; version=1',
-      'Authorization': 'Bearer ' + lockAccessToken
-    },
+      "Content-Type": "application/json",
+      Accept: "application/vnd.lockstate+json; version=1",
+      Authorization: "Bearer " + lockAccessToken
+    }
   })
     .then(checkStatus)
     .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(err => console.error(err));
-
-
-
+    .then(json => {
+      guestAccessLock(json.data.id);
+      return json.data.id;
+    })
+    .then(id => res.send(200, id))
+    .catch(err => {
+      console.error(err);
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
