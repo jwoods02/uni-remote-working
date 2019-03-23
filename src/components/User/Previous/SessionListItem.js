776626c81a1905485d5c2bc3f197385b769f9e7c
elliottdaviess <elliott_davies@live.co.uk>
2019-03-23 11:34:50 +0000

    Getting location details for each session

--- src/components/User/Previous/SessionListItem.js
+++ src/components/User/Previous/SessionListItem.js
@@ -12,69 +12,96 @@ import {
 const { width } = Dimensions.get("window");
 
 export default class SessionListItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      location: {}
+    };
+  }
+
+  async componentDidMount() {
+    const doc = await this.props.session.data().access_code.location.get();
+
+    if (doc.exists) {
+      console.log("doc exists");
+      this.setState({
+        location: doc.data(),
+        isLoading: false
+      });
+    } else {
+      console.log("No such document!");
+    }
+  }
+
   render() {
     // console.log("PROPS SECONDS", this.props.session.data().start.seconds);
-    return (
-      <View style={{ marginTop: 40, paddingHorizontal: 20 }}>
-        <Text style={{ color: "#8A54A2", fontSize: 24, fontWeight: "700" }}>
-          {/* {this.props.session.data().start} */}
-        </Text>
-        <Text style={{ fontWeight: "100", marginTop: 10 }}>
-          A new selection of workspaces verified for quality & comfort
-        </Text>
-        <Text style={{ fontWeight: "100", marginTop: 10 }}>
-          Requested:
-          {" " +
-            new Date(
-              this.props.session.data().access_code.requested.seconds * 1000
-            ).toLocaleTimeString("en-US") +
-            " on " +
-            new Date(
-              this.props.session.data().access_code.requested.seconds * 1000
-            ).toLocaleDateString("en-UK")}
-        </Text>
-        <Text style={{ fontWeight: "100", marginTop: 10 }}>
-          Session Start:
-          {" " +
-            new Date(
-              this.props.session.data().start.seconds * 1000
-            ).toLocaleTimeString("en-US") +
-            " on " +
-            new Date(
-              this.props.session.data().start.seconds * 1000
-            ).toLocaleDateString("en-UK")}{" "}
-        </Text>
-        <Text style={{ fontWeight: "100", marginTop: 10 }}>
-          Session End:
-          {" " +
-            new Date(
-              this.props.session.data().start.end * 1000
-            ).toLocaleTimeString("en-US") +
-            " on " +
-            new Date(
-              this.props.session.data().start.end * 1000
-            ).toLocaleDateString("en-UK")}{" "}
-        </Text>
-        <Text style={{ fontWeight: "100", marginTop: 10 }}>
-          A new selection of workspaces verified for quality & comfort A new
-          selection of workspaces verified for quality & comfort
-        </Text>
-        <View style={{ width: width - 40, height: 100, marginTop: 20 }}>
-          <Image
-            style={{
-              flex: 1,
-              height: null,
-              width: null,
-              resizeMode: "cover",
-              borderRadius: 5,
-              borderWidth: 1,
-              borderColor: "#dddddd"
-            }}
-            source={require("../../../../assets/locationD.jpg")}
-          />
+    if (this.state.loading) {
+      return (
+        <View style={styles.container}>
+          <Text>Loading</Text>
+          <ActivityIndicator size="large" color="rgba(130,4,150, 0.4)" />
         </View>
-      </View>
-    );
+      );
+    } else {
+      return (
+        <View style={{ marginTop: 40, paddingHorizontal: 20 }}>
+          <Text style={{ color: "#8A54A2", fontSize: 24, fontWeight: "700" }}>
+            {this.state.location.title}
+          </Text>
+
+          <View style={{ width: width - 40, height: 100, marginTop: 20 }}>
+            <Image
+              style={{
+                flex: 1,
+                height: null,
+                width: null,
+                resizeMode: "cover",
+                borderRadius: 5,
+                borderWidth: 1,
+                borderColor: "#dddddd"
+              }}
+              source={{ uri: this.state.location.image }}
+            />
+          </View>
+
+          <Text style={{ fontWeight: "100", marginTop: 10 }}>
+            Requested:
+            {" " +
+              new Date(
+                this.props.session.data().access_code.requested.seconds * 1000
+              ).toLocaleTimeString("en-US") +
+              " on " +
+              new Date(
+                this.props.session.data().access_code.requested.seconds * 1000
+              ).toLocaleDateString("en-UK")}
+          </Text>
+          <Text style={{ fontWeight: "100", marginTop: 10 }}>
+            Session Start:
+            {" " +
+              new Date(
+                this.props.session.data().start.seconds * 1000
+              ).toLocaleTimeString("en-US") +
+              " on " +
+              new Date(
+                this.props.session.data().start.seconds * 1000
+              ).toLocaleDateString("en-UK")}{" "}
+          </Text>
+          <Text style={{ fontWeight: "100", marginTop: 10 }}>
+            Session End:
+            {" " +
+              new Date(
+                this.props.session.data().end.seconds * 1000
+              ).toLocaleTimeString("en-US") +
+              " on " +
+              new Date(
+                this.props.session.data().end.seconds * 1000
+              ).toLocaleDateString("en-UK")}{" "}
+          </Text>
+        </View>
+      );
+    }
   }
 }
 
