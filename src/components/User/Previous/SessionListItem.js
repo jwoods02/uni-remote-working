f8a220a168c7a5edf7c1916157887ea990b03e65
elliottdaviess <elliott_davies@live.co.uk>
2019-03-23 11:05:58 +0000

    Passing session props to Session items
    need to get Location details

--- src/components/User/Previous/SessionListItem.js
+++ src/components/User/Previous/SessionListItem.js
@@ -13,15 +13,53 @@ const { width } = Dimensions.get("window");
 
 export default class SessionListItem extends Component {
   render() {
+    // console.log("PROPS SECONDS", this.props.session.data().start.seconds);
     return (
       <View style={{ marginTop: 40, paddingHorizontal: 20 }}>
         <Text style={{ color: "#8A54A2", fontSize: 24, fontWeight: "700" }}>
-          Featured: Location D
+          {/* {this.props.session.data().start} */}
         </Text>
         <Text style={{ fontWeight: "100", marginTop: 10 }}>
           A new selection of workspaces verified for quality & comfort
         </Text>
-        <View style={{ width: width - 40, height: 200, marginTop: 20 }}>
+        <Text style={{ fontWeight: "100", marginTop: 10 }}>
+          Requested:
+          {" " +
+            new Date(
+              this.props.session.data().access_code.requested.seconds * 1000
+            ).toLocaleTimeString("en-US") +
+            " on " +
+            new Date(
+              this.props.session.data().access_code.requested.seconds * 1000
+            ).toLocaleDateString("en-UK")}
+        </Text>
+        <Text style={{ fontWeight: "100", marginTop: 10 }}>
+          Session Start:
+          {" " +
+            new Date(
+              this.props.session.data().start.seconds * 1000
+            ).toLocaleTimeString("en-US") +
+            " on " +
+            new Date(
+              this.props.session.data().start.seconds * 1000
+            ).toLocaleDateString("en-UK")}{" "}
+        </Text>
+        <Text style={{ fontWeight: "100", marginTop: 10 }}>
+          Session End:
+          {" " +
+            new Date(
+              this.props.session.data().start.end * 1000
+            ).toLocaleTimeString("en-US") +
+            " on " +
+            new Date(
+              this.props.session.data().start.end * 1000
+            ).toLocaleDateString("en-UK")}{" "}
+        </Text>
+        <Text style={{ fontWeight: "100", marginTop: 10 }}>
+          A new selection of workspaces verified for quality & comfort A new
+          selection of workspaces verified for quality & comfort
+        </Text>
+        <View style={{ width: width - 40, height: 100, marginTop: 20 }}>
           <Image
             style={{
               flex: 1,
