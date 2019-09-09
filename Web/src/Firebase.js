ab931a55cae2eb875f3401bae9b04b21d1f90617
James Woods <jwwoods01@gmail.com>
2019-09-09 16:17:20 +0100

    Removed secret in Firebase.js

--- Web/src/Firebase.js
+++ Web/src/Firebase.js
@@ -4,12 +4,12 @@ import firestore from "firebase/firestore";
 // const settings = { timestampsInSnapshots: true };
 
 const config = {
-  apiKey: "AIzaSyB3eOEQaPomF624RwDBl3bmO97guiN-TRs",
+  apiKey: "",
   authDomain: "remoteruralworking.firebaseapp.com",
   databaseURL: "https://remoteruralworking.firebaseio.com",
   projectId: "remoteruralworking",
   storageBucket: "remoteruralworking.appspot.com",
-  messagingSenderId: "397948314964"
+  messagingSenderId: ""
 };
 firebase.initializeApp(config);
 
